e4141abb0fca3a1d06232a708e24e3ad9dd834e7
Eroh Oghenemaro Divine <maroeroh@gmail.com>
2026-02-07 15:13:01 +0100

    added FNG

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -6,7 +6,7 @@ const API = "https://eroh-maro-student-portal-backend.vercel.app";
 
 /* Case-insensitive regex */
 const matricRegex =
-  /^(FOS|COS|FOE|FOA|PHC|BMS)\/\d{2}\/\d{2}\/\d{4,6}$/i;
+  /^(FOS|FNG|COS|FOE|FOA|PHC|BMS)\/\d{2}\/\d{2}\/\d{4,6}$/i;
 
 const Signup = () => {
   const navigate = useNavigate();
